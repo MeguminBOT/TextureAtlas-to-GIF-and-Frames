e30e492a0679877f65a1f948f0bdd1242cff4b10
MeguminBOT <discord.meguminbot@gmail.com>
2024-04-12 11:34:32 +0200

    Revert "Merge pull request #2 from cloudcloudskies/main"
    This reverts commit e0a428e90070686db647536c7d299bcf33fdbdc0, reversing
    changes made to 42f73eae688309d583397dd77a2dece0ac34e645.

--- TextureAtlas to GIF and Frames.py
+++ TextureAtlas to GIF and Frames.py
@@ -22,7 +22,7 @@ def check_for_updates(current_version):
             result = messagebox.askyesno("Update available", "An update is available. Do you want to download it now?")
             if result:
                 print("User chose to download the update.")
-                webbrowser.open('https://github.com/MeguminBOT/TextureAtlas-to-GIF-and-Frames/releases/latest')
+                webbrowser.open('https://github.com/MeguminBOT/TextureAtlas-to-GIF-and-Frames/releases')
                 sys.exit()
             else:
                 print("User chose not to download the update.")
@@ -47,7 +47,7 @@ def count_png_files(directory):
 def sanitize_filename(name):
     return re.sub(r'[\\/:*?"<>|]', '_', name)
 
-def extract_sprites(atlas_path, xml_path, output_dir, create_gif, create_webp, selected_sprites=None):
+def extract_sprites(atlas_path, xml_path, output_dir, create_gif, create_webp):
     atlas = Image.open(atlas_path)
     tree = ET.parse(xml_path)
     root = tree.getroot()
@@ -55,8 +55,6 @@ def extract_sprites(atlas_path, xml_path, output_dir, create_gif, create_webp, s
 
     for sprite in root.findall('SubTexture'):
         name = sprite.get('name')
-        if selected_sprites and name not in selected_sprites:
-            continue
         x, y, width, height = map(int, (sprite.get(attr) for attr in ('x', 'y', 'width', 'height')))
         frameX = int(sprite.get('frameX', 0))
         frameY = int(sprite.get('frameY', 0))
@@ -85,20 +83,13 @@ def extract_sprites(atlas_path, xml_path, output_dir, create_gif, create_webp, s
 
     if create_gif:
         for animation_name, images in animations.items():
-            images[0].save(os.path.join(output_dir, f"_{animation_name}.gif"), save_all=True, append_images=images[1:], disposal=2, optimize=False, duration=1000/frame_rate, loop=0)
+            images[0].save(os.path.join(output_dir, f"_{animation_name}.gif"), save_all=True, append_images=images[1:], disposal=2, optimize=False, duration=1000/24, loop=0)
 
     if create_webp:
         for animation_name, images in animations.items():
-            images[0].save(os.path.join(output_dir, f"_{animation_name}.webp"), save_all=True, append_images=images[1:], disposal=2, optimize=False, duration=1000/frame_rate, loop=0)
+            images[0].save(os.path.join(output_dir, f"_{animation_name}.webp"), save_all=True, append_images=images[1:], disposal=2, optimize=False, duration=1000/24, loop=0)
 
-    if create_gif:
-        for animation_name in animations:
-            folder_path = os.path.join(output_dir, animation_name)
-            for filename in os.listdir(folder_path):
-                file_path = os.path.join(folder_path, filename)
-                os.remove(file_path)
-
-def process_directory(input_dir, output_dir, progress_var, tk_root, create_gif, create_webp, frame_rate, selected_sprites=None):
+def process_directory(input_dir, output_dir, progress_var, tk_root, create_gif, create_webp):
     progress_var.set(0)
     total_files = count_png_files(input_dir)
     progress_bar["maximum"] = total_files
@@ -111,7 +102,7 @@ def process_directory(input_dir, output_dir, progress_var, tk_root, create_gif, 
             if os.path.isfile(xml_path):
                 sprite_output_dir = os.path.join(output_dir, filename.rsplit('.', 1)[0])
                 os.makedirs(sprite_output_dir, exist_ok=True)
-                extract_sprites(os.path.join(input_dir, filename), xml_path, sprite_output_dir, create_gif, create_webp, selected_sprites)
+                extract_sprites(os.path.join(input_dir, filename), xml_path, sprite_output_dir, create_gif, create_webp)
                 progress_var.set(progress_var.get() + 1)
                 tk_root.update_idletasks()
 
@@ -123,8 +114,6 @@ root.geometry("640x360")
 
 input_dir = tk.StringVar()
 output_dir = tk.StringVar()
-frame_rate_var = tk.DoubleVar(value=24)
-
 progress_var = tk.DoubleVar()
 progress_bar = ttk.Progressbar(root, length=200, variable=progress_var)
 progress_bar.pack()
@@ -135,11 +124,6 @@ input_button.pack()
 output_button = tk.Button(root, text="Select save directory", command=lambda: select_directory(output_dir))
 output_button.pack()
 
-frame_rate_label = tk.Label(root, text="Frame Rate (fps):")
-frame_rate_label.pack()
-frame_rate_entry = tk.Entry(root, textvariable=frame_rate_var)
-frame_rate_entry.pack()
-
 create_gif = tk.BooleanVar()
 gif_checkbox = tk.Checkbutton(root, text="Create GIFs for each animation", variable=create_gif)
 gif_checkbox.pack()
@@ -148,10 +132,7 @@ create_webp = tk.BooleanVar()
 webp_checkbox = tk.Checkbutton(root, text="Create WebPs for each animation", variable=create_webp)
 webp_checkbox.pack()
 
-def process():
-    process_directory(input_dir.get(), output_dir.get(), progress_var, root, create_gif.get(), create_webp.get(), frame_rate_var.get())
-
-process_button = tk.Button(root, text="Start process", command=process)
+process_button = tk.Button(root, text="DO MAGIC!!", command=lambda: process_directory(input_dir.get(), output_dir.get(), progress_var, root, create_gif.get(), create_webp.get()))
 process_button.pack()
 
 author_label = tk.Label(root, text="Tool written by AutisticLulu")
