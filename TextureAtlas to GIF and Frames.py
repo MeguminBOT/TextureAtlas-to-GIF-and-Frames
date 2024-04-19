24fd807c1a955a294c87f6c59491d20d4bd9d023
MeguminBOT <discord.meguminbot@gmail.com>
2024-04-19 12:31:36 +0200

    Refactor

--- TextureAtlas to GIF and Frames.py
+++ TextureAtlas to GIF and Frames.py
@@ -8,6 +8,7 @@ import webbrowser
 import requests
 import sys
 import concurrent.futures
+from pathlib import Path
 
 ## Update Checking
 def check_for_updates(current_version):
@@ -36,12 +37,6 @@ current_version = '1.4.1'
 check_for_updates(current_version)
 
 ## File processing
-def count_png_files(directory):
-    return sum(1 for filename in os.listdir(directory) if filename.endswith('.png'))
-
-def sanitize_filename(name):
-    return re.sub(r'[\\/:*?"<>|]', '_', name).rstrip()
-
 def select_directory(variable, label):
     directory = filedialog.askdirectory()
     if directory:
@@ -98,26 +93,24 @@ def on_double_click_xml(evt):
             new_window.lift()
 
     tk.Button(new_window, text="OK", command=store_input).pack()
+
 user_fps_and_delay = {}
 
-def process_directory(input_dir, output_dir, progress_var, tk_root, create_gif, create_webp, set_framerate, set_loopdelay):
+def count_png_files(directory):
+    return len(list(Path(directory).glob('*.png')))
+
+def process_files(input_dir, output_dir, progress_var, tk_root, create_gif, create_webp, set_framerate, set_loopdelay):
+    input_dir = Path(input_dir)
+    output_dir = Path(output_dir)
+
     progress_var.set(0)
-    total_files = count_png_files(input_dir)
+    png_files = list(input_dir.glob('*.png'))
+    total_files = len(png_files)
     progress_bar["maximum"] = total_files
 
     max_workers = os.cpu_count() // 2
     with concurrent.futures.ThreadPoolExecutor(max_workers=max_workers) as executor:
-        futures = []
-        for filename in os.listdir(input_dir):
-            if filename.endswith('.png'):
-                xml_filename = filename.rsplit('.', 1)[0] + '.xml'
-                xml_path = os.path.join(input_dir, xml_filename)
-
-                if os.path.isfile(xml_path):
-                    sprite_output_dir = os.path.join(output_dir, filename.rsplit('.', 1)[0])
-                    os.makedirs(sprite_output_dir, exist_ok=True)
-                    future = executor.submit(extract_sprites, os.path.join(input_dir, filename), xml_path, sprite_output_dir, create_gif, create_webp, set_framerate, set_loopdelay)
-                    futures.append(future)
+        futures = [submit_task(executor, png_file, output_dir, create_gif, create_webp, set_framerate, set_loopdelay) for png_file in png_files]
 
         for future in concurrent.futures.as_completed(futures):
             try:
@@ -136,55 +129,66 @@ def process_directory(input_dir, output_dir, progress_var, tk_root, create_gif, 
 
     messagebox.showinfo("Information","Finished processing all files.")
 
+def submit_task(executor, png_file, output_dir, create_gif, create_webp, set_framerate, set_loopdelay):
+    xml_file = png_file.with_suffix('.xml')
+    if xml_file.exists():
+        sprite_output_dir = output_dir / png_file.stem
+        sprite_output_dir.mkdir(parents=True, exist_ok=True)
+        return executor.submit(extract_sprites, str(png_file), str(xml_file), str(sprite_output_dir), create_gif, create_webp, set_framerate, set_loopdelay)
+
 ## Extraction logic
+def process_sprite(sprite, atlas, output_dir):
+    name = sprite.get('name')
+    x, y, width, height = map(int, (sprite.get(attr) for attr in ('x', 'y', 'width', 'height')))
+    frameX = int(sprite.get('frameX', 0))
+    frameY = int(sprite.get('frameY', 0))
+    frameWidth = int(sprite.get('frameWidth', width))
+    frameHeight = int(sprite.get('frameHeight', height))
+    rotated = sprite.get('rotated', 'false') == 'true'
+
+    sprite_image = atlas.crop((x, y, x + width, y + height))
+    if rotated: 
+        sprite_image = sprite_image.rotate(90, expand=True)
+
+    frame_image = Image.new('RGBA', (frameWidth, frameHeight))
+    frame_image.paste(sprite_image, (-frameX, -frameY))
+
+    if frame_image.mode != 'RGBA':
+        frame_image = frame_image.convert('RGBA')
+
+    folder_name = re.sub(r'\d+$', '', name).rstrip()
+    sprite_folder = os.path.join(output_dir, folder_name)
+    os.makedirs(sprite_folder, exist_ok=True)
+
+    frame_image.save(os.path.join(sprite_folder, f"{name}.png"))
+
+    return folder_name, frame_image
+
+def create_animation(animation_name, images, output_dir, file_extension, set_framerate, set_loopdelay):
+    fps, delay = user_fps_and_delay.get(animation_name, (set_framerate, set_loopdelay))
+    durations = [round(1000/fps)] * len(images)
+    durations[-1] = delay
+    images[0].save(os.path.join(output_dir, f"_{animation_name}.{file_extension}"), save_all=True, append_images=images[1:], disposal=2, duration=durations, loop=0, lossless=True)
+
 def extract_sprites(atlas_path, xml_path, output_dir, create_gif, create_webp, set_framerate, set_loopdelay):
     try:
-        atlas = Image.open(atlas_path)
-        tree = ET.parse(xml_path)
-        root = tree.getroot()
-        animations = {}
+        with Image.open(atlas_path) as atlas:
+            tree = ET.parse(xml_path)
+            root = tree.getroot()
+            animations = {}
 
-        for sprite in root.findall('SubTexture'):
-            name = sprite.get('name')
-            x, y, width, height = map(int, (sprite.get(attr) for attr in ('x', 'y', 'width', 'height')))
-            frameX = int(sprite.get('frameX', 0))
-            frameY = int(sprite.get('frameY', 0))
-            frameWidth = int(sprite.get('frameWidth', width))
-            frameHeight = int(sprite.get('frameHeight', height))
-            rotated = sprite.get('rotated', 'false') == 'true'
+            for sprite in root.findall('SubTexture'):
+                folder_name, frame_image = process_sprite(sprite, atlas, output_dir)
+                if create_gif or create_webp:
+                    animations.setdefault(folder_name, []).append(frame_image)
 
-            sprite_image = atlas.crop((x, y, x + width, y + height))
-            if rotated: 
-                sprite_image = sprite_image.rotate(90, expand=True)
+            if create_gif:
+                for animation_name, images in animations.items():
+                    create_animation(animation_name, images, output_dir, 'gif', set_framerate, set_loopdelay)
 
-            frame_image = Image.new('RGBA', (frameWidth, frameHeight))
-            frame_image.paste(sprite_image, (-frameX, -frameY))
-
-            if frame_image.mode != 'RGBA':
-                frame_image = frame_image.convert('RGBA')
-
-            folder_name = re.sub(r'\d+$', '', name).rstrip()
-            sprite_folder = os.path.join(output_dir, folder_name)
-            os.makedirs(sprite_folder, exist_ok=True)
-
-            frame_image.save(os.path.join(sprite_folder, f"{name}.png"))
-
-            if create_gif or create_webp:
-                animations.setdefault(folder_name, []).append(frame_image)
-
-        if create_gif:
-            for animation_name, images in animations.items():
-                fps, delay = user_fps_and_delay.get(animation_name, (set_framerate, set_loopdelay))
-                durations = [round(1000/fps)] * len(images)
-                durations[-1] = delay
-                images[0].save(os.path.join(output_dir, f"_{animation_name}.gif"), save_all=True, append_images=images[1:], disposal=2, optimize=False, duration=durations, loop=0)
-
-        if create_webp:
-            for animation_name, images in animations.items():
-                fps, delay = user_fps_and_delay.get(animation_name, (set_framerate, set_loopdelay))
-                durations = [round(1000/fps)] * len(images)
-                durations[-1] = delay
-                images[0].save(os.path.join(output_dir, f"_{animation_name}.webp"), save_all=True, append_images=images[1:], disposal=2, duration=durations, loop=0, lossless=True)
+            if create_webp:
+                for animation_name, images in animations.items():
+                    create_animation(animation_name, images, output_dir, 'webp', set_framerate, set_loopdelay)
 
     except ET.ParseError:
         raise ET.ParseError(f"Badly formatted XML file:\n{xml_path}")
@@ -250,7 +254,7 @@ loopdelay_label.pack()
 loopdelay_entry = tk.Entry(root, textvariable=set_loopdelay)
 loopdelay_entry.pack()
 
-process_button = tk.Button(root, text="Start process", cursor="hand2", command=lambda: process_directory(input_dir.get(), output_dir.get(), progress_var, root, create_gif.get(), create_webp.get(), set_framerate.get(), set_loopdelay.get()))
+process_button = tk.Button(root, text="Start process", cursor="hand2", command=lambda: process_files(input_dir.get(), output_dir.get(), progress_var, root, create_gif.get(), create_webp.get(), set_framerate.get(), set_loopdelay.get()))
 process_button.pack(pady=8)
 
 author_label = tk.Label(root, text="Tool written by AutisticLulu")
