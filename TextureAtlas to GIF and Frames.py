2d4a511cb4d083b0b8a18103992389476f44090b
AutisticLulu <discord.meguminbot@gmail.com>
2024-11-27 12:25:20 +0100

    Mention that alpha threshold may be the culprit on "Coordinate x is less than blah".

--- TextureAtlas to GIF and Frames.py
+++ TextureAtlas to GIF and Frames.py
@@ -764,7 +764,7 @@ def extract_sprites(atlas_path, metadata_path, output_dir, create_gif, create_we
     except Exception as e:
         if "Coordinate '" in str(e) and "' is less than '" in str(e):
             sprites_failed += 1
-            raise Exception(f"XML or TXT frame dimension data doesn't match the spritesheet dimensions.\n\nError: {str(e)}\n\nFile: {metadata_path}")
+            raise Exception(f"XML or TXT frame dimension data doesn't match the spritesheet dimensions.\n\nError: {str(e)}\n\nFile: {metadata_path}\n\nThis error can also occur from Alpha Threshold being set too high.")
         elif "'NoneType' object is not subscriptable" in str(e):
             sprites_failed += 1
             raise Exception(f"XML or TXT frame dimension data doesn't match the spritesheet dimensions.\n\nError: {str(e)}\n\nFile: {metadata_path}")
