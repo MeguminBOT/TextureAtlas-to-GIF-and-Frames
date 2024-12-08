e464819dec087030d88eb392ff5f4958a3112323
AutisticLulu <discord.meguminbot@gmail.com>
2024-12-08 14:06:02 +0100

    Update Main.py

--- adobe_spritemap/Main.py
+++ adobe_spritemap/Main.py
@@ -11,12 +11,11 @@ if __name__ == "__main__":
         "bicubic": Image.BICUBIC,
     }
 
-    animation_dir = r"E:\GitHub\TextureAtlas-to-GIF-and-Frames\experimental features\Atlas\madness v1.1\assets\shared\images\characters\hank"
+    animation_dir = r"E:\GitHub\TextureAtlas-to-GIF-and-Frames\testsprite\Spritemap\images\handhank"
     animation_dir_formatted = animation_dir.replace("\\", "/")
-    animation_dir = "./test2"
     canvas_size = (4000, 4000)
     resample = Image.BICUBIC
     background_color = (0, 0, 0, 0)  # Transparent RGBA
 
     anim = Animation(animation_dir_formatted, canvas_size, resample)
-    anim.render_to_png_sequence(output_dir="./testResult")
+    anim.render_to_png_sequence(output_dir="./testsprite/Spritemap/testResult")
