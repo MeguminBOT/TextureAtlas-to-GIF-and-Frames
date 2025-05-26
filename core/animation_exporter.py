90c04513eb69b8799ef35e769eb7afaf288feda2
Jsfasdf250 <53478680+Jsfasdf250@users.noreply.github.com>
2025-05-26 00:43:34 -0400

    Revamp GIF generation
    - GIF processing is now done entirely by Wand
    - Duplicate frames are removed once again
    - consolidated filename parameter

--- core/animation_exporter.py
+++ core/animation_exporter.py
@@ -2,8 +2,8 @@ import os
 import numpy
 from PIL import Image
 from PIL.PngImagePlugin import PngInfo
-from wand.image import Image as WandImg
 from wand.color import Color
+from wand.image import Image as WandImg
 
 # Import our own modules
 from utils.utilities import Utilities
@@ -19,17 +19,17 @@ class AnimationExporter:
             Version string to include in metadata.
         scale_image_func (str):
             Function to scale images before saving.
-        quant_frames (str):
-            Stores quantized frames for optimized GIF generation.
 
     Methods:
-        save_animations(image_tuples, spritesheet_name, animation_name, settings) -> int
+        save_animations(image_tuples, filename, settings) -> int
             Processes and saves the animation in the specified format (GIF, WebP, or APNG).
-        save_gif(images, spritesheet_name, animation_name, fps, delay, period, scale, threshold, settings)
+        remove_dups(animation)
+            Removes duplicate frames from a Wand animation, merging delays as needed.
+        save_gif(images, filename, fps, delay, period, scale, threshold, settings)
             Saves the animation as a GIF file.
-        save_webp(images, spritesheet_name, animation_name, fps, delay, period, scale, settings)
+        save_webp(images, filename, fps, delay, period, scale, settings)
             Saves the animation as a WebP file.
-        save_apng(images, spritesheet_name, animation_name, fps, delay, period, scale, settings)
+        save_apng(images, filename, fps, delay, period, scale, settings)
             Saves the animation as an APNG file.
     """
 
@@ -37,7 +37,6 @@ class AnimationExporter:
         self.output_dir = output_dir
         self.current_version = current_version
         self.scale_image = scale_image_func
-        self.quant_frames = quant_frames
 
     def save_animations(self, image_tuples, spritesheet_name, animation_name, settings):
         anims_generated = 0
@@ -60,26 +59,34 @@ class AnimationExporter:
                 new_frame.paste(frame)
                 images[index] = new_frame
 
+        filename = Utilities.format_filename(
+            settings.get('prefix'),
+            spritesheet_name,
+            animation_name,
+            settings.get('filename_format'),
+            settings.get('replace_rules')
+        )
+
         if animation_format == 'GIF':
             self.save_gif(
-                images, spritesheet_name, animation_name, fps, delay, period,
+                images, filename, fps, delay, period,
                 scale, threshold, settings
             )
         elif animation_format == 'WebP':
             self.save_webp(
-                images, spritesheet_name, animation_name, fps, delay, period,
+                images, filename, fps, delay, period,
                 scale, settings
             )
         elif animation_format == 'APNG':
             self.save_apng(
-                images, spritesheet_name, animation_name, fps, delay, period,
+                images, filename, fps, delay, period,
                 scale, settings
             )
 
         anims_generated += 1
         return anims_generated
 
-    def save_webp(self, images, spritesheet_name, animation_name, fps, delay, period, scale, settings):
+    def save_webp(self, images, filename, fps, delay, period, scale, settings):
         min_x, min_y, max_x, max_y = float('inf'), float('inf'), 0, 0
 
         for frame in images:
@@ -112,15 +119,8 @@ class AnimationExporter:
 
         durations[-1] += delay
         durations[-1] += max(period - sum(durations), 0)
-
-        formatted_webp_name = Utilities.format_filename(
-            settings.get('prefix'),
-            spritesheet_name,
-            animation_name,
-            settings.get('filename_format'),
-            settings.get('replace_rules')
-        )
-        webp_filename = os.path.join(self.output_dir, f"{formatted_webp_name}.webp")
+        
+        webp_filename = os.path.join(self.output_dir, f"{filename}.webp")
 
         final_images[0].save(
             webp_filename,
@@ -133,88 +133,76 @@ class AnimationExporter:
         )
         print(f"Saved WEBP animation: {webp_filename}")
 
-    def save_gif(self, images, spritesheet_name, animation_name, fps, delay, period, scale, threshold, settings):
-        for frame in images:
-            alpha = frame.getchannel('A')
-            if threshold == 1:
-                alpha = alpha.point(lambda i: i >= 255 and 255)
-            else:
-                alpha = alpha.point(lambda i: i > 255 * threshold and 255)
-            frame.putalpha(alpha)
+    def remove_dups(self, animation):
+        animation.iterator_reset()
+        while animation.iterator_next():
+            index = animation.iterator_get()
+            if animation.get_image_distortion(animation.sequence[index - 1], metric='absolute') == 0:
+                delay = animation.delay
+                animation.image_remove()
+                animation.iterator_set(index - 1)
+                animation.delay += delay
 
-        min_x, min_y, max_x, max_y = float('inf'), float('inf'), 0, 0
-        for frame in images:
-            bbox = frame.getbbox()
-            if bbox is None:
-                continue
-            min_x = min(min_x, bbox[0])
-            min_y = min(min_y, bbox[1])
-            max_x = max(max_x, bbox[2])
-            max_y = max(max_y, bbox[3])
-
-        if min_x > max_x:
-            return
-
-        final_images = []
-        if settings.get('crop_option') == 'None':
-            final_images = list(map(lambda x: self.scale_image(x, scale), images))
-        else:
-            for frame in images:
-                cropped_frame = frame.crop((min_x, min_y, max_x, max_y))
-                final_images.append(self.scale_image(cropped_frame, scale))
-
+    def save_gif(self, images, filename, fps, delay, period, scale, threshold, settings):
         durations = []
-        var_delay = settings.get('var_delay')
-        if var_delay:
-            for index in range(len(final_images)):
+        if settings.get('var_delay'):
+            for index in range(len(images)):
                 durations.append(round((index + 1) * 1000 / fps, -1) - round(index * 1000 / fps, -1))
         else:
-            durations = [round(1000 / fps, -1)] * len(final_images)
+            durations = [round(1000 / fps, -1)] * len(images)
         durations[-1] += delay
         durations[-1] += max(round(period, -1) - sum(durations), 0)
 
-        wand_frames = []
-        for pil_frame in final_images:
-            arr = numpy.array(pil_frame)
-            with WandImg.from_array(arr) as wand_frame:
-                wand_frame.background_color = Color('None')
-                wand_frame.alpha_channel = 'background'
-                if wand_frame.colors > 256:
-                    wand_frame.quantize(number_colors=256, colorspace_type='undefined', dither=False)
-                # Wand uses centiseconds for frame durations, so we divide by 10.
-                wand_frame.delay = int(durations[len(wand_frames)] / 10)
-                wand_frame.dispose = 'background'
-                wand_frames.append(wand_frame.clone())
-                wand_frame.destroy()
-
-        formatted_gif_name = Utilities.format_filename(
-            settings.get('prefix'),
-            spritesheet_name,
-            animation_name,
-            settings.get('filename_format'),
-            settings.get('replace_rules')
-        )
-        gif_filename = os.path.join(self.output_dir, f"{formatted_gif_name}.gif")
-
-        if not wand_frames:
-            print(f"Warning: No frames to save for GIF: {gif_filename}")
-            return
-        with WandImg() as animation:
-            for frame in wand_frames:
-                animation.sequence.append(frame)
-            if len(animation.sequence) > 1:
-                del animation.sequence[0]
-                animation.type = 'optimize'
+        width, height = images[0].size
+        left, upper, right, lower = width, height, 0, 0
+        with WandImg(width=width, height=height) as animation:
+            animation.image_remove()
+            for index, pil_frame in enumerate(images):
+                arr = numpy.array(pil_frame)
+                with WandImg.from_array(arr) as wand_frame:
+                    if threshold == 1:
+                        wand_frame.negate(channel='alpha')
+                        wand_frame.threshold(0,channel='alpha')
+                        wand_frame.negate(channel='alpha')
+                    else:
+                        wand_frame.threshold(threshold,channel='alpha')
+                    wand_frame.background_color = Color('None')
+                    wand_frame.alpha_channel = 'background'
+                    wand_frame.trim(color='None')
+                    wand_frame.delay = int(durations[index] / 10)
+                    wand_frame.dispose = 'background'
+                    animation.sequence.append(wand_frame)
+                    if settings.get('crop_option') != 'None':
+                        if (wand_frame.size > (1, 1) or wand_frame.maxima > 0):
+                            left = min(wand_frame.page_x, left)
+                            upper = min(wand_frame.page_y, upper)
+                            right = max(wand_frame.page_x + wand_frame.width, right)
+                            lower = max(wand_frame.page_y + wand_frame.height, lower)
+            self.remove_dups(animation)
+            animation.iterator_reset()
+            for i in range(len(animation.sequence)):
+                animation.iterator_set(i)
+                animation.quantize(number_colors=256, colorspace_type='undefined', dither=False)
+            # We remove duplicate frames twice because different frames may become the same after quantization.
+            self.remove_dups(animation)
+            animation.coalesce()
+            for i in range(len(animation.sequence)):
+                animation.iterator_set(i)
+                if settings.get('crop_option') != 'None':
+                    animation.crop(left, upper, right, lower)
+                animation.sample(width=int(animation.width * abs(scale)), height=int(animation.height * abs(scale)))
+                if scale < 0:
+                    animation.flop()
+            gif_filename = os.path.join(self.output_dir, f"{filename}.gif")
+            animation.type = 'optimize'
             animation.format = 'gif'
             animation.loop = 0
             animation.options['comment'] = f'GIF generated by: TextureAtlas to GIF and Frames v{self.current_version}'
             animation.save(filename=gif_filename)
+            
+            print(f"Saved GIF animation: {gif_filename}")
 
-        for frame in wand_frames:
-            frame.destroy()
-        print(f"Saved GIF animation: {gif_filename}")
-
-    def save_apng(self, images, spritesheet_name, animation_name, fps, delay, period, scale, settings):
+    def save_apng(self, images, filename, fps, delay, period, scale, settings):
         min_x, min_y, max_x, max_y = float('inf'), float('inf'), 0, 0
 
         for frame in images:
@@ -247,15 +235,8 @@ class AnimationExporter:
 
         durations[-1] += int(delay)
         durations[-1] += max(int(round(period)) - sum(durations), 0)
-
-        formatted_apng_name = Utilities.format_filename(
-            settings.get('prefix'),
-            spritesheet_name,
-            animation_name,
-            settings.get('filename_format'),
-            settings.get('replace_rules')
-        )
-        apng_filename = os.path.join(self.output_dir, f"{formatted_apng_name}.png")
+        
+        apng_filename = os.path.join(self.output_dir, f"{filename}.png")
 
         metadata = PngInfo()
         metadata.add_text("Comment", f'APNG generated by TextureAtlas to GIF and Frames v{self.current_version}')
